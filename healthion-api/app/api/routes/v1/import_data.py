bd70946b1529ba8697aa378c93754f05d4104c50
czajkub <czajkub@gmail.com>
2025-10-30 09:47:04 +0100

    adapt import router

--- healthion-api/app/api/routes/v1/import_data.py
+++ healthion-api/app/api/routes/v1/import_data.py
@@ -9,15 +9,8 @@ from app.database import DbSession
 router = APIRouter()
 
 
-@router.post("/apple/import/auto-health-export")
-async def import_data(
-    request: Request,
-    db: DbSession,
-    user_id: str = Depends(get_current_user_id)
-) -> UploadDataResponse:
-    """Import health data from file upload or JSON."""
+async def get_content_type(request: Request) -> tuple[str, str]:
     content_type = request.headers.get("content-type", "")
-    
     if "multipart/form-data" in content_type:
         form = await request.form()
         file = form.get("file")
@@ -30,29 +23,31 @@ async def import_data(
         body = await request.body()
         content_str = body.decode("utf-8")
     
+    return content_str, content_type
+
+
+@router.post("/import/apple/auto-health-export")
+async def import_data_auto_health_export(
+    request: Request,
+    db: DbSession,
+    user_id: str = Depends(get_current_user_id),
+    content: tuple[str, str] = Depends(get_content_type)
+) -> UploadDataResponse:
+    """Import health data from file upload or JSON."""
+    
+    content_str, content_type = content[0], content[1]
     return await ae_import_service.import_data_from_request(db, content_str, content_type, user_id)
 
 
-@router.post("/apple/import/healthion")
+@router.post("/import/apple/healthion")
 async def import_data_healthion(
         request: Request,
         db: DbSession,
-        user_id: str = Depends(get_current_user_id)
+        user_id: str = Depends(get_current_user_id),
+        content: tuple[str, str] = Depends(get_content_type)
 ) -> UploadDataResponse:
     """Import health data from file upload or JSON."""
-    content_type = request.headers.get("content-type", "")
-
-    if "multipart/form-data" in content_type:
-        form = await request.form()
-        file = form.get("file")
-        if not file:
-            return UploadDataResponse(response="No file found")
-
-        content_str = await file.read()
-        content_str = content_str.decode("utf-8")
-    else:
-        body = await request.body()
-        content_str = body.decode("utf-8")
-
+    
+    content_str, content_type = content[0], content[1]
     return await hk_import_service.import_data_from_request(db, content_str, content_type, user_id)
 
